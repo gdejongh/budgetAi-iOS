e5930fb0f7729278e822f4fe141f3bf2f7cee78e
Gabe <dejonghgabe1@gmail.com>
2026-03-03 17:13:16 -0500

    Initial Commit

---
+++ ai envelope budget/ai_envelope_budgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ai_envelope_budgetApp.swift
+//  ai envelope budget
+//
+//  Created by Gabe DeJongh on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct ai_envelope_budgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
